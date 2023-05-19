35f73d4259e7b2d5e2ac5a53d1c75af2b25170d7
Carlosgoes0 <carlos.goes@sptech.school>
2023-05-18 22:16:53 -0300

    Implementando API WEB DATA VIZ

--- bd/TABELAS_NOVO_CAMINHO.sql
+++ bd/TABELAS_NOVO_CAMINHO.sql
@@ -2,42 +2,29 @@ CREATE DATABASE NovoCaminho;
 USE NovoCaminho;
 
 
-create table Empresa(
-idEmpresa int primary key auto_increment,
-nomeFantasia varchar(45),
-razaoSocial varchar (45),
-email varchar(45) not null,
-telefone varchar(12),
-cep char(9),
-cnpj char(16) not null,
-rua varchar(25),
-numero varchar(8), 
-complemento varchar (25)
-);
-
-desc Empresa;
-
-
-create table Usuario(
-idUsuario int auto_increment,
-nome varchar(45),
-email varchar(45) not null,
-senha varchar(20) not null,
-cpf char (12) not null,
-fkEmpresa int,
-constraint fkEmpresaUsuario foreign key (fkEmpresa) references Empresa(idEmpresa),
-constraint pkComposta primary key (idUsuario, fkEmpresa)
+create table tb_usuario(
+idUsuario int primary key auto_increment,
+Nome varchar(45),
+Contato char (11),
+Cpf char(11),
+Estado varchar(3),
+Email varchar(45),
+Senha int
 );
 
 
+create table tb_doacao (
+idDoacao int primary key auto_increment,
+Valor int,
+fkUsuario int,
+fkProjetos int,
+constraint fkUsuario foreign key (fkUsuario) references tb_usuario (idUsuario),
+constraint fkProjetos foreign key (fkProjetos) references tb_projetos (idProjetos)
+);
 
-insert into Empresa values
-(null, '', '', '', '', '', '', '', '', null);
 
-insert into Usuario values
-(null, '', '', '', '', '');
-
-insert into p values
-(null, 'coca'),
-(null, 'pepsi');
+create table tb_projetos (
+idProjetos int primary key auto_increment,
+Nome varchar(45)
+);
 
